8623bbafc1a6fee8a571968004ab366d536c6036
YUNSIK.CHO <postop09@naver.com>
2022-12-27 14:19:43 +0900

    #1 [개발환경 설정] - 폴더 구조 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/components/layout/Layout";
+import Layout from "./components/layout/Layout";
 import FraudPrevention from "./pages/FraudPrevention";
 
 function App() {
