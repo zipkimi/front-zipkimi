acec8e7c0a21f026c7056c47e55f4f497ded380d
CHO YUN SIK <postop09@naver.com>
2023-01-13 15:55:51 +0900

    프리티어 설치

--- src/pages/Fraud/Prevent/index.tsx
+++ src/pages/Fraud/Prevent/index.tsx
@@ -6,66 +6,66 @@ import { list } from "../../../mockData";
 import OriginalBtn from "../../../components/OriginalBtn";
 
 const Index = () => {
-  const navigate = useNavigate();
+	const navigate = useNavigate();
 
-  // TODO - API 연결
-  //  상세조회를 어느 주소로 보내느냐에 따라 달라짐.
-  const onDetail = (id: number) => {
-    navigate(`/${id}`);
-  };
+	// TODO - API 연결
+	//  상세조회를 어느 주소로 보내느냐에 따라 달라짐.
+	const onDetail = (id: number) => {
+		navigate(`/${id}`);
+	};
 
-  const onRegister = () => {
-    navigate(`/register`);
-  };
+	const onRegister = () => {
+		navigate(`/register`);
+	};
 
-  const tempData = {
-    id: 999,
-    title: "제목입니다.",
-    contents:
-      "사람이 행복하기 위해서는 일을 할 수 있어야 할 뿐 아니라 자신이 한 일을 제대로 판단할 수 있어야 한다.",
-    writer: "yunsik cho",
-    regDt: "2022-12-25",
-    comments: 130,
-    likes: 2900,
-  };
+	const tempData = {
+		id: 999,
+		title: "제목입니다.",
+		contents:
+			"사람이 행복하기 위해서는 일을 할 수 있어야 할 뿐 아니라 자신이 한 일을 제대로 판단할 수 있어야 한다.",
+		writer: "yunsik cho",
+		regDt: "2022-12-25",
+		comments: 130,
+		likes: 2900,
+	};
 
-  return (
-    <Wrapper>
-      <HiddenH3>사기예방법 공유</HiddenH3>
-      <OriginalBtn onClick={onRegister}>글쓰기</OriginalBtn>
-      <Ul>
-        <Card {...tempData} onDetail={onDetail} />
-        {list.map((item, index) => {
-          return <Card key={index} onDetail={onDetail} {...item} />;
-        })}
-      </Ul>
-    </Wrapper>
-  );
+	return (
+		<Wrapper>
+			<HiddenH3>사기예방법 공유</HiddenH3>
+			<OriginalBtn onClick={onRegister}>글쓰기</OriginalBtn>
+			<Ul>
+				<Card {...tempData} onDetail={onDetail} />
+				{list.map((item, index) => {
+					return <Card key={index} onDetail={onDetail} {...item} />;
+				})}
+			</Ul>
+		</Wrapper>
+	);
 };
 
 export default Index;
 
 const Wrapper = styled.section`
-  padding: 2rem 2rem;
-  max-width: 800px;
-  margin: auto;
-  @media screen and (max-width: 480px) {
-    padding: 2rem 1rem;
-  }
+	padding: 2rem 2rem;
+	max-width: 800px;
+	margin: auto;
+	@media screen and (max-width: 480px) {
+		padding: 2rem 1rem;
+	}
 
-  > button {
-    float: right;
-  }
+	> button {
+		float: right;
+	}
 `;
 
 const HiddenH3 = styled.h3`
-  ${({ theme }) => theme.TEXT.hide};
+	${({ theme }) => theme.TEXT.hide};
 `;
 
 const Ul = styled.ul`
-  clear: both;
+	clear: both;
 
-  li:not(li:last-child) {
-    margin-bottom: 1rem;
-  }
+	li:not(li:last-child) {
+		margin-bottom: 1rem;
+	}
 `;
